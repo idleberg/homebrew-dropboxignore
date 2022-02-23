ccc99768ec9c00f2cc2c0820c920c68d402358b6
Jan T. Sott <git@idleberg.com>
2022-02-23 21:10:44 +0100

    fix formula

--- Formula/dropboxignore.rb
+++ Formula/dropboxignore.rb
@@ -1,24 +1,30 @@
-class Dbxcli < Formula
-  desc "Command-line client for Dropbox built using the Go SDK"
-  homepage "https://github.com/dropbox/dbxcli"
+class Dropboxignore < Formula
+  desc "CLI tool for ignoring files in Dropbox"
+  homepage "https://dropboxignore.simakis.me/"
+  url "https://github.com/sp1thas/dropboxignore/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "033fa1ca2593489772ea3af97a665f5de4a1751fce6bb8b246322e8835cb402d"
+  license "GPL-3.0-only"
 
-  if OS.mac?
-    @@os = "darwin"
-    checksum = "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
-  elsif OS.linux?
-    @@os = "linux"
-    checksum = "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
+  bottle do
+    sha256 arm64_monterey: "24854522e2901befeb323be066c744ab4471920495b7a91280e9d05c9bc3b9e7"
+    sha256 arm64_big_sur:  "e062925ce6b559649d5574f2ee4a709611df4a9a54f3396cf706c2a399cc747f"
+    sha256 monterey:       "923df44c0bbcdcb70df775092fb6aacb3c7c4740a12d40ce6f5ad4a8dd7ea91f"
+    sha256 big_sur:        "0c4b7d1c41dbd920e192711e8ed1200db46c30be141aaaeb606c41718d0c2a79"
+    sha256 catalina:       "09e77aa3db2cf8a702effbebbbf83f7a2f860b0d5db6bcf37549edb7db5438a7"
+    sha256 mojave:         "a6ab99c751a03e11e2ace660ad9325a9fe4262598f284c0fb87626778383e29d"
+    sha256 high_sierra:    "a0461ecc678e5cb65a901bd39dbd7f0f8015a29ed605e6cf28f1315d5c347ecb"
+    sha256 x86_64_linux:   "32ec309e47f9cc195c0bc8c9bccdf1169ff91e211991eeb1fb04d91872c3be51"
   end
 
-  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-#{@@os}-amd64"
-  sha256 checksum
+  depends_on "diffutils"
+  depends_on "grep"
 
   def install
-    mv "dbxcli-#{@@os}-amd64", "dbxcli"
-    bin.install "dbxcli"
+    # Do not use 'make install' as it uses hardcoded destination
+    bin.install "bin/dropboxignore.sh" => "dropboxignore"
   end
 
   test do
-    system "#{bin}/dbxcli", "--help"
+    assert_match "dropboxignore: v#{version}", shell_output("#{bin}/dropboxignore version")
   end
 end
